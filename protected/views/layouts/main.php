63af2eea32cd7d26101206e01b4633be9409178d
minh10huy <todachuy2406@gmail.com>
2012-12-17 17:31:29 +0700

    17/12/2012

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -2,30 +2,54 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="language" content="en" />
 
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
+<!-- blueprint CSS framework -->
+<link rel="stylesheet" type="text/css"
+	href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css"
+	media="screen, projection" />
+<link rel="stylesheet" type="text/css"
+	href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css"
+	media="print" />
+<!--[if lt IE 8]>
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
 	<![endif]-->
 
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+<link rel="stylesheet" type="text/css"
+	href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+<link rel="stylesheet" type="text/css"
+	href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+<link rel="stylesheet" type="text/css"
+	href="<?php echo Yii::app()->request->baseUrl; ?>/css/menu.css" />
 
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
 <body>
-
-<div class="container" id="page">
-
 	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
+		<a href="<?php echo Yii::app()->createUrl('site/index'); ?>" id='logo'><span></span>
+		</a>
+	</div>
+	<!-- header -->
+	<div id="mainmenu">
+		<div id="menu">
+			<?php
+				$this->widget('zii.widgets.CMenu', array(
+					'items'=>array(
+						array('label'=>'Trang Chủ', 'url'=>array('/site/index')),
+						array('label'=>'Tìm Việc', 'url'=>array('/jobsearch/search')),
+						array('label'=>'Tạo Hồ Sơ', 'url'=>array('/createCV/create')),
+						array('label'=>'Quản Lý Hồ Sơ', 'url'=>array('/manageCV/viewCV')),
+						array('label'=>'Đăng Ký', 'url'=>array('/user/login'), 'itemOptions'=>array('class'=>'menu_right'),),
+						array('label'=>'Đăng Nhập', 'url'=>array('/user/login'), 'itemOptions'=>array('class'=>'menu_right'),),
+				),
+			));	?>
+		</div>
+	</div>
+	<div class="container" id="page">
 
+		<!--
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
@@ -37,23 +61,28 @@
 			),
 		)); ?>
 	</div><!-- mainmenu -->
-	<?php if(isset($this->breadcrumbs)):?>
+
+		<?php if(isset($this->breadcrumbs)):?>
 		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
 			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
+		)); ?>
+		<!-- breadcrumbs -->
+		<?php endif?>
 
-	<?php echo $content; ?>
+		<?php echo $content; ?>
 
-	<div class="clear"></div>
+		<div class="clear"></div>
 
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
-	</div><!-- footer -->
+		<div id="footer">
+			Copyright &copy;
+			<?php echo date('Y'); ?>
+			by My Company.<br /> All Rights Reserved.<br />
+			<?php echo Yii::powered(); ?>
+		</div>
+		<!-- footer -->
 
-</div><!-- page -->
+	</div>
+	<!-- page -->
 
 </body>
 </html>
